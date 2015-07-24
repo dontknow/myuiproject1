a5a593b5fd1a71301ddcaf000a1e47b7ace2abbe
dontknow <gsumanthkr@gmail.com>
2015-07-23 20:45:54 -0400

    Level 01 complete

--- app.js
+++ app.js
@@ -2,13 +2,29 @@
     var app = angular.module('store', []);
     
     app.controller('StoreController', function() {
-        this.product = gem;
+        this.products = gems;
     });
     
-    var gem = {
+    var gems = [{
         name: "Kohinoor",
         price: 2.95,
-        description: 'Not a gem'
-    }
+        description: 'Not a gem',
+        canPurchase: false,
+        soldOut: true
+    },
+                {
+        name: "Quartz",
+        price: 149.95,
+        description: 'Not a gem',
+        canPurchase: true,
+        soldOut: false
+    },
+                {
+        name: "Ruby",
+        price: 49.95,
+        description: 'Not a gem',
+        canPurchase: false,
+        soldOut: false
+    }]
 })();
 
